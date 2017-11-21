5a63ebaa4a9a1fdf05ea3a2622a4cbd05b1aa268
Grzegorz Kossakowski <gkk@stripe.com>
2017-11-20 22:00:14 -0800

    WIP: got scala_import to work here

--- example-lib/test/Test.scala
+++ example-lib/test/Test.scala
@@ -4,4 +4,7 @@ class TestSuite extends FlatSpec {
   "things" should "work" in {
     assert(Foo.message == "hello world")
   }
+  "things" should "not work" in {
+    assert(Foo.message == "hell2o world")
+  }
 }
